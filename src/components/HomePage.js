18a084fd115a6ef5c47000e94b260c34d93d4c97
Blake Danson <blakedanson@Blakes-MacBook-Pro.local>
2017-02-20 19:25:56 -0800

    (debug) Tries to fetch backend

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,11 +2,14 @@ import React from 'react';
 import {Link} from 'react-router';
 
 const HomePage = () => {
+  fetch("http://localhost:3333").then(function(data){
+    console.log(data,'data');
+  });
   return (
     <div>
       <h1>React Slingshot</h1>
 
-      <h2>Get Started</h2>
+      <h2>Hello World!!!!!!</h2>
       <ol>
         <li>Review the <Link to="fuel-savings">demo app</Link></li>
         <li>Remove the demo and start coding: npm run remove-demo</li>
