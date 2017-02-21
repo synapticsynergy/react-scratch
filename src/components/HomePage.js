2cedb4a2cbc403c87a506e84b7e723a34caa5052
mousezero <rm@mousezero.com>
2017-02-20 20:37:14 -0800

    (feat) Can use POST now

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,11 +2,24 @@ import React from 'react';
 import {Link} from 'react-router';
 
 const HomePage = () => {
-  fetch("http://localhost:3333").then(function(data){
-    return data.json()
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      // 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      'Content-Type': 'application/json; charset=UTF-8'
+    },
+    body: JSON.stringify({
+      name: 'starbucks'
+    })
+  };
+  fetch("http://localhost:3333", fetchOptions)
+  .then(function(data){
+    return data.json();
   }).then(function(json){
     console.log(json);
   });
+  console.log('test');
   return (
     <div>
       <h1>React Slingshot</h1>
